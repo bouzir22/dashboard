1076c458876d1c1dd689c19946e37dbaf63160cf
skyzo110 <mohamed.guendia@esen.tn>
2023-10-14 14:16:40 +0100

    update jobs

--- react-ui/src/views/extra/Jobs.js
+++ react-ui/src/views/extra/Jobs.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import { Card, Table,Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Card, Table } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle, faEdit, faTrashAlt } from '@fortawesome/free-regular-svg-icons';
 
 const Jobs = () => {
-//fetch jobs
-
-
   return (
     <Card>
       <Card.Header>
-        <Card.Title as="h5">Hover Table</Card.Title>
-        <span className="d-block m-t-5">
-          use props <code>hover</code> with <code>Table</code> component
-        </span>
+        <Card.Title as="h5">Job List</Card.Title>
       </Card.Header>
       <Card.Body>
-      <Link to="/opportunity/add">
-  <Button className="btn-rounded text-capitalize" variant="success">
-    apply
-  </Button>
-</Link>
         <Table responsive hover>
           <thead>
             <tr>
@@ -27,29 +17,66 @@ const Jobs = () => {
               <th>Job Title</th>
               <th>Publish Date</th>
               <th>Deadline</th>
-              <th>availability</th>
-              <th>Number of Applications</th>
+              <th>Availability</th>
+              <th>Actions</th>
             </tr>
           </thead>
           <tbody>
             <tr>
               <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
+              <td>Job 1</td>
+              <td>01/01/2023</td>
+              <td>01/31/2023</td>
+              <td>Open</td>
+              <td>
+                <a href="/check" className="btn-circle">
+                  <FontAwesomeIcon icon={faCheckCircle} size="lg" className="text-success mx-2" />
+                </a>
+                <a href="/edit" className="btn-circle">
+                  <FontAwesomeIcon icon={faEdit} size="lg" className="text-primary mx-2" />
+                </a>
+                <a href="/delete" className="btn-circle">
+                  <FontAwesomeIcon icon={faTrashAlt} size="lg" className="text-danger mx-2" />
+                </a>
+              </td>
             </tr>
             <tr>
               <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
+              <td>Job 2</td>
+              <td>02/01/2023</td>
+              <td>02/28/2023</td>
+              <td>Closed</td>
+              <td>
+                <a href="/check" className="btn-circle">
+                  <FontAwesomeIcon icon={faCheckCircle} size="lg" className="text-success mx-2" />
+                </a>
+                <a href="/edit" className="btn-circle">
+                  <FontAwesomeIcon icon={faEdit} size="lg" className="text-primary mx-2" />
+                </a>
+                <a href="/delete" className="btn-circle">
+                  <FontAwesomeIcon icon={faTrashAlt} size="lg" className="text-danger mx-2" />
+                </a>
+              </td>
             </tr>
             <tr>
               <th scope="row">3</th>
-              <td>Larry</td>
-              <td>the Bird</td>
-              <td>@twitter</td>
+              <td>Job 3</td>
+              <td>03/01/2023</td>
+              <td>03/15/2023</td>
+              <td>Open</td>
+              <td>
+                <a href="/check" className="btn-circle">
+                  <FontAwesomeIcon icon={faCheckCircle} size="lg" className="text-success mx-2" />
+                </a>
+                <a href="/edit" className="btn-circle">
+                  <FontAwesomeIcon icon={faEdit} size="lg" className="text-primary mx-2" />
+                </a>
+                <a href="/delete" className="btn-circle">
+                  <FontAwesomeIcon icon={faTrashAlt} size="lg" className="text-danger mx-2" />
+                </a>
+              </td>
             </tr>
+            {/* Add more job rows as needed */}
           </tbody>
         </Table>
       </Card.Body>
